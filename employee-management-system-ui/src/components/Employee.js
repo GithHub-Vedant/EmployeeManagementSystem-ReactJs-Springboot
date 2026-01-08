0e4ffebcebe532180f548ddad55d36bdb85468e1
GithHub-Vedant <vvligade2024@gmail.com>
2026-01-08 21:57:18 +0530

    Update Employee.js

--- employee-management-system-ui/src/components/Employee.js
+++ employee-management-system-ui/src/components/Employee.js
@@ -1,113 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import EmployeeService from "../services/EmployeeService";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const UpdateEmployee = () => {
-  const { id } = useParams();
+const Employee = ({ employee, deleteEmployee }) => {
   const navigate = useNavigate();
-  const [employee, setEmployee] = useState({
-    id: id,
-    firstName: "",
-    lastName: "",
-    emailId: "",
-    contact: "",
-  });
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setEmployee({ ...employee, [e.target.name]: value });
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await EmployeeService.getEmployeeById(employee.id);
-        setEmployee(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  const updateEmployee = (e) => {
+  const editEmployee = (e, id) => {
     e.preventDefault();
-    console.log(employee);
-    EmployeeService.updateEmployee(employee, id)
-      .then((response) => {
-        navigate("/employeeList");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    navigate(`/editEmployee/${id}`);
   };
 
   return (
-    <div className="flex max-w-2xl mx-auto shadow border-b">
-      <div className="px-8 py-8">
-        <div className="font-thin text-2xl tracking-wider">
-          <h1>Update Employee</h1>
-        </div>
-        <div className="items-center justify-center h-14 w-full my-4">
-          <label className="block text-gray-600 text-sm font-normal">
-            First Name
-          </label>
-          <input
-            type="text"
-            name="firstName"
-            value={employee.firstName}
-            onChange={(e) => handleChange(e)}
-            className="h-10 w-96 border mt-2 px-2 py-2"></input>
-        </div>
-        <div className="items-center justify-center h-14 w-full my-4">
-          <label className="block text-gray-600 text-sm font-normal">
-            Last Name
-          </label>
-          <input
-            type="text"
-            name="lastName"
-            value={employee.lastName}
-            onChange={(e) => handleChange(e)}
-            className="h-10 w-96 border mt-2 px-2 py-2"></input>
-        </div>
-        <div className="items-center justify-center h-14 w-full my-4">
-          <label className="block text-gray-600 text-sm font-normal">
-            Email
-          </label>
-          <input
-            type="email"
-            name="emailId"
-            value={employee.emailId}
-            onChange={(e) => handleChange(e)}
-            className="h-10 w-96 border mt-2 px-2 py-2"></input>
-        </div>
-        <div className="items-center justify-center h-14 w-full my-4">
-          <label className="block text-gray-600 text-sm font-normal">
-            Contact
-          </label>
-          <input
-            type="text"
-            name="contact"
-            value={employee.contact}
-            onChange={(e) => handleChange(e)}
-            className="h-10 w-96 border mt-2 px-2 py-2"></input>
-        </div>
-
-        <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-          <button
-            onClick={updateEmployee}
-            className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
-            Update
-          </button>
-          <button
-            onClick={() => navigate("/employeeList")}
-            className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">
-            Cancel
-          </button>
-        </div>
-      </div>
-    </div>
+    <tr key={employee.id}>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.firstName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.lastName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.emailId}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.contact}</div>
+      </td>
+      <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+        <a
+          onClick={(e, id) => editEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">
+          Edit
+        </a>
+        <a
+          onClick={(e, id) => deleteEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer">
+          Delete
+        </a>
+      </td>
+    </tr>
   );
 };
 
-export default UpdateEmployee;
+export default Employee;
